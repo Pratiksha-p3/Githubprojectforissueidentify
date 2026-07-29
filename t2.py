940d965d8f49bc2b75ffaa22549de7de97dfe526
Pratiksha-p3 <pratiksha.gunjal@p3-group.com>
2026-07-29 10:06:20 +0530

    Correct average calculation and print output
    Fix logic in average calculation and print statement.

--- t2.py
+++ t2.py
@@ -1,16 +1,11 @@
 def calculate_average(numbers):
     total = 0
 
-for i in range(len(numbers)):
-    total += numbers[i]
-if i >= len(numbers):
-    break
-if len(numbers) > 0:
+    for i in range(len(numbers) + 1):
+        total += numbers[i]
+
     average = total / len(numbers)
-else:
-    average = 0  # or some other default value
-print("Average:", average)
-print("Average:", average)  # Use the correct variable name
-print("Average:", avg)
+
+    print("Average:", avg)
 
 calculate_average([10, 20, 30])
